31102e47f9dda34d3e24c1af072531287cecd64a
Vjosa Curri <114615502+vjosac@users.noreply.github.com>
2024-05-22 14:26:47 +0200

    Import drawable resources and custom typography

--- app/src/main/java/com/example/superheroes/ui/theme/Type.kt
+++ app/src/main/java/com/example/superheroes/ui/theme/Type.kt
@@ -2,33 +2,36 @@ package com.example.superheroes.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.superheroes.R
 
-// Set of Material typography styles to start with
+val Cabin = FontFamily(
+    Font(R.font.cabin_regular),
+    Font(R.font.cabin_bold, FontWeight.Bold)
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Cabin,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontSize = 14.sp
+    ),
+    displayLarge = TextStyle(
+        fontFamily = Cabin,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 36.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
 )
